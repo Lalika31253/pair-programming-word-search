525aaffabcc5725d40e2787ee91a960107a69056
Lalika31253 <Lalika31253@gmail.com>
2023-11-18 16:42:37 -0700

    Add Word Search function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,19 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
+    const horizontalJoin = letters.map(ls => ls.join(''));
+
+    const verticalJoin = letters[0].map(i => letters.map(row => row[i]).join(''));
+
+    for (l of horizontalJoin) {
+        if (l.includes(word) || l.split('').reverse().join('').includes(word)) {
+            return true;
+        }
+    }
+
+    for (r of verticalJoin) {
+        if (r.includes(word)) return true;
+    }
+
+    return false;
+}
 module.exports = wordSearch
